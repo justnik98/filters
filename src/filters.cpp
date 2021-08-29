20075ba37577863f03286423f412db4149ea929d
Nikita Yankovskii <yannik98@yandex.ru>
2021-08-29 23:23:11 +0300

    added namespace

--- src/filters.cpp
+++ src/filters.cpp
@@ -5,46 +5,49 @@
 #include <cmath>
 #include "filters.hpp"
 
-double MedianFilter::filter(double new_val) {
-    if (data.size() < size) {
-        data.push_back(new_val);
-    } else {
-        data[ind++ % size] = new_val;
+namespace filter {
+
+    double MedianFilter::filter(double new_val) {
+        if (data.size() < size) {
+            data.push_back(new_val);
+        } else {
+            data[ind++ % size] = new_val;
+        }
+        auto v = data;
+        auto m = v.begin() + v.size() / 2;
+        std::nth_element(v.begin(), m, v.end());
+        return v[v.size() / 2];
     }
-    auto v = data;
-    auto m = v.begin() + v.size() / 2;
-    std::nth_element(v.begin(), m, v.end());
-    return v[v.size() / 2];
-}
 
-MedianFilter::MedianFilter(size_t size) : size(size) {
-    data.reserve(size);
-}
-
-double RunningAvg::filter(double new_val) {
-    if (data.size() < size) {
-        data.push_back(new_val);
-    } else {
-        data[ind++ % size] = new_val;
+    MedianFilter::MedianFilter(size_t size) : size(size) {
+        data.reserve(size);
     }
-    double mean = 0;
-    for (auto &&v: data) {
-        mean += v;
+
+    double RunningAvg::filter(double new_val) {
+        if (data.size() < size) {
+            data.push_back(new_val);
+        } else {
+            data[ind++ % size] = new_val;
+        }
+        double mean = 0;
+        for (auto &&v: data) {
+            mean += v;
+        }
+        return mean / static_cast<double>(data.size());
     }
-    return mean / static_cast<double>(data.size());
-}
 
-RunningAvg::RunningAvg(size_t size) : size(size) {
-    data.reserve(size);
-}
-
-double ExpSmooth::filter(double new_val) {
-    if (std::isnan(last_val)) {
-        last_val = new_val;
+    RunningAvg::RunningAvg(size_t size) : size(size) {
+        data.reserve(size);
     }
-    auto res = new_val * coef + (1 - coef) * last_val;
-    last_val = res;
-    return res;
-}
 
-ExpSmooth::ExpSmooth(double coef) : coef(coef) {}
+    double ExpSmooth::filter(double new_val) {
+        if (std::isnan(last_val)) {
+            last_val = new_val;
+        }
+        auto res = new_val * coef + (1 - coef) * last_val;
+        last_val = res;
+        return res;
+    }
+
+    ExpSmooth::ExpSmooth(double coef) : coef(coef) {}
+}
--- src/filters.hpp
+++ src/filters.hpp
@@ -7,44 +7,45 @@
 
 #include <vector>
 
-class IFilter {
-public:
-    virtual double filter(double new_val) = 0;
-};
+namespace filter {
+    class IFilter {
+    public:
+        virtual double filter(double new_val) = 0;
+    };
 
-class MedianFilter : public IFilter {
-public:
-    MedianFilter(size_t size);
+    class MedianFilter : public IFilter {
+    public:
+        MedianFilter(size_t size);
 
-    double filter(double new_val) final;
+        double filter(double new_val) final;
 
-private:
-    std::vector<double> data;
-    std::size_t size;
-    std::size_t ind = 0;
-};
+    private:
+        std::vector<double> data;
+        std::size_t size;
+        std::size_t ind = 0;
+    };
 
-class RunningAvg : public IFilter {
-public:
-    explicit RunningAvg(size_t size);
+    class RunningAvg : public IFilter {
+    public:
+        explicit RunningAvg(size_t size);
 
-    double filter(double new_val) final;
+        double filter(double new_val) final;
 
-private:
-    std::vector<double> data;
-    std::size_t size;
-    std::size_t ind = 0;
-};
+    private:
+        std::vector<double> data;
+        std::size_t size;
+        std::size_t ind = 0;
+    };
 
-class ExpSmooth : public IFilter {
-public:
-    explicit ExpSmooth(double coef);
+    class ExpSmooth : public IFilter {
+    public:
+        explicit ExpSmooth(double coef);
 
-    double filter(double new_val) final;
+        double filter(double new_val) final;
 
-private:
-    double coef = 1;
-    double last_val = std::numeric_limits<double>::quiet_NaN();
-};
-
+    private:
+        double coef = 1;
+        double last_val = std::numeric_limits<double>::quiet_NaN();
+    };
+}
 #endif //FILTERS_FILTERS_HPP
